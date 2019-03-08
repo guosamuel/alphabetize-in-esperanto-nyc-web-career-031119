9c3b4bbfa936e5035ac0cce87dd9e927bb5c256a
Samuel Guo <guosamuel1114@gmail.com>
2019-03-08 15:55:30 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,10 @@
 def alphabetize(arr)
-  
+  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
   arr.sort_by do |sentence|
     sentence.split("").map do |character|
       ESPERANTO_ALPHABET.index(character)
     end
   end
+  
 end
